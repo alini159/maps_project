ee1186ba179b63446eb54d9a058020c7f7b43842
Alini Rodrigues Ferreira <alini.ferreira.mtm@outlook.com>
2023-09-06 09:37:18 -0400

    adjust layout

--- app/src/main/java/com/example/maps_project/utils/Constants.java
+++ app/src/main/java/com/example/maps_project/utils/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
     public static final String BASE_URL_VIA_CEP = "https://viacep.com.br/ws/";
     public static final String BASE_URL_API_CEP = "https://cdn.apicep.com/file/apicep/";
     public static final String BASE_URL_AWESOME_API = "https://cep.awesomeapi.com.br/json/";
-
     public static final String DEBUG = "DEBUG_LOG";
     public static final String ERROR = "ERROR_LOG";
 
